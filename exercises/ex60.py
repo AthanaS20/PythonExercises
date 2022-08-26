e3aa28a19cffc040ee6f13fe33020952f116ff7a
AthanaS20 <athanasilva95@gmail.com>
2022-08-26 17:56:04 -0300

    Update ex60.py

--- exercises/ex60.py
+++ exercises/ex60.py
@@ -1,8 +1,12 @@
-#from math import factorial
+from math import factorial
 numero = int(input('Digite um valor para ver o fatorial do número:'))
 contador = numero
-
+fatorial = factorial(numero)
+print('Calculando fatorial de {}!'.format(numero))
 while contador > 0:
-    print('{} '.format(contador), end= '')
-    print(' x ' if contador < 1 else ' = ', end='')
+    print('{}'.format(contador), end= '')
+    print(' x ' if contador > 1 else ' = ', end='')
     contador -= 1
+print('{}'.format(fatorial))
+
+    
