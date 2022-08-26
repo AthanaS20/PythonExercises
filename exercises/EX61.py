10fc2326b04a3f38c92602a8429113b0301717bb
AthanaS20 <athanasilva95@gmail.com>
2022-08-26 18:19:13 -0300

    Create EX61.py

---
+++ exercises/EX61.py
@@ -0,0 +1,13 @@
+print('=' * 20)
+print('10 TERMOS DE UMA PA')
+print('=' * 20)
+PrimeiroTermo = int(input('Digite o PRIMEIRO TERMO:'))
+razao = int(input('Digite a RAZÃO:'))
+termo = PrimeiroTermo
+contador = 1
+while contador <= 10:
+    print('{}'.format(termo), end=' -> ')
+    termo += termo
+    contador += 1
+    
+print('FINISH')
