8a00b46c07edbd31c7563cc2cc3b1082e73f5e51
AthanaS20 <athanasilva95@gmail.com>
2022-10-09 17:56:36 -0300

    add exercise

--- exercises/EX74.py
+++ exercises/EX74.py
@@ -1,12 +1,16 @@
 from random import sample
 
 numeros = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-maior = max(sample(numeros,5))
 
-print('Os valores sorteados foram:')
-print(sample(numeros,5))
+samples = sample(numeros, 5)
 
-print(max(numeros))
+print(f'Os valores sorteados foram: {samples}')
+
+print(f'O maior entre eles é {max(samples)}')
+
+
+
+
 
 
 
